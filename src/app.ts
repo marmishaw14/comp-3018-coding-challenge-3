f5df8fe99380480ba269d9d3afe7328a019c74fe
McCauley Armishaw <151598523+marmishaw14@users.noreply.github.com>
2026-02-24 17:53:49 -0600

    feat(app): add health check route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import express from "express";
+import { HealthCheckResponse } from "./api/v1/models/healthCheckModel";
 import productRoutes from "../src/api/v1/routes/productRoutes";
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/api/v1/health", (req, res) => {
+    const healthData: HealthCheckResponse = {
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0",
+    };
+
+    res.json(healthData);
+})
 
 app.use("/api/v1", productRoutes);
 
