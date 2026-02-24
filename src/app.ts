af26d86f8926e1b420df39a66c1eeb34bb8146b2
McCauley Armishaw <151598523+marmishaw14@users.noreply.github.com>
2026-02-24 17:19:52 -0600

    feat(app): update app file to include initial routing

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import productRoutes from "../src/api/v1/routes/productRoutes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1", productRoutes);
 
 const PORT = process.env.PORT || 3023;
 app.listen(PORT, () => {
